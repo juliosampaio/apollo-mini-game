c72c5bf1d8a84fe1f1295bd7f95cdf4194a32d16
Julio Sampaio <juliodssampaio@gmail.com>
2020-04-19 12:10:25 -0300

    feature: adjusts console styles

--- src/components/display/index.js
+++ src/components/display/index.js
@@ -2,6 +2,14 @@ import React from "react";
 
 import styles from "./style.module.css";
 
-const Display = () => <div className={styles.display}></div>;
+const Display = () => (
+  <div className={styles.display}>
+    <div className={styles.left}>
+      <div className={styles.score}></div>
+      <div className={styles.canvas}></div>
+    </div>
+    <div className={styles.right}></div>
+  </div>
+);
 
 export default Display;
