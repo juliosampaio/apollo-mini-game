bdaecea020656bcfc4df60e4ebc493a438920452
Julio Sampaio <jsampaio@uolinc.com>
2020-10-27 22:53:11 -0300

    apply major refactor on context

--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import { useEngineActions } from '../../hooks';
 import Button from '../button';
 import styles from './style.module.css';
 
-const Controls = () => (
-  <div className={styles.buttonsContainer}>
-    <div className={styles.row}>
-      <Button size='m' labels={['left', 'height']} />
-      <Button size='m' labels={['right', 'speed']} />
-      <Button size='s' labels={['on/off']} />
-      <Button size='l' labels={['rotate', 'direction']} />
+const Controls = () => {
+  const { togglePower, toggleSound } = useEngineActions();
+  return (
+    <div className={styles.buttonsContainer}>
+      <div className={styles.row}>
+        <Button size='m' labels={['left', 'height']} />
+        <Button size='m' labels={['right', 'speed']} />
+        <Button size='s' labels={['on/off']} onClick={togglePower} />
+        <Button size='l' labels={['rotate', 'direction']} />
+      </div>
+      <div className={styles.row}>
+        <Button size='m' labels={['down', 'mode']} />
+        <Button size='s' labels={['mute']} onClick={toggleSound} />
+      </div>
+      <div className={styles.row}>
+        <Button size='s' labels={['start/p']} />
+        <Button size='s' labels={['reset']} />
+      </div>
     </div>
-    <div className={styles.row}>
-      <Button size='m' labels={['down', 'mode']} />
-      <Button size='s' labels={['mute']} />
-    </div>
-    <div className={styles.row}>
-      <Button size='s' labels={['start/p']} />
-      <Button size='s' labels={['reset']} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Controls;
--- src/hooks/useEngine.js
+++ src/hooks/useEngine.js
@@ -1,9 +1,21 @@
 import { useContext } from 'react';
 import { Engine } from '../context';
-export const useEngine = () => {
-  const context = useContext(Engine.EngineContext);
+export const useEngineState = () => {
+  const context = useContext(Engine.Context);
   if (!context) throw new Error('No EngineProvider found');
   return context;
+};
+
+export const useEngineActions = () => {
+  const context = useContext(Engine.ActionsContext);
+  if (!context) throw new Error('No EngineProvider found');
+  return context;
+};
+
+export const useEngine = () => {
+  const state = useEngineState();
+  const actions = useEngineActions();
+  return [state, actions];
 };
 
 // import React, { useEffect, useCallback } from 'react';
